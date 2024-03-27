8b75de8c05f22a05d1e51efd0fc1566ce4761086
Your Name git config --global user.email you@example.com <you@example.com>
2024-03-27 15:41:38 +0600

    BookCard create

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,21 +1,27 @@
 
+import { CiStar } from "react-icons/ci";
 
-
-const BookCard = ({ book}) => {
-    const {bookId, bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing} = book
+const BookCard = ({ book }) => {
+    const { bookId, bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing } = book
     return (
-        <div className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900 dark:bg-gray-50">
+        <div className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900 dark:bg-gray-50 w-[360px] gap-4 p-6 border-2">
             <div className="flex justify-center items-center mt-4">
-            <img role="presentation" className="object-cover  rounded h-44 bg-gray-500 dark:bg-gray-500" src={image} />F
+                <img role="presentation" className="rounded h-44 bg-gray-500 dark:bg-gray-500 shadow-2xl w-[130px]" src={image} />
             </div>
+
             <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{bookName}</h3>
-                <span className="text-xs text-gray-400 dark:text-gray-600">{author}</span>
-                <p>{category}</p>
-            </div>
-            <div>
+                <div className="flex items-center">
+                    {
+                        tags.map(tag => <p key={tag} className=" bg-[#23BE0A0D] mr-2 p-2 rounded-lg">{tag}</p>)
+                    }
+                </div>
                 <div>
-                    
+                    <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{bookName}</h3>
+                    <span className="text-xs text-gray-400 dark:text-gray-600">{author}</span>
+                </div> <hr className="mb-4" />
+                <div className="flex justify-between">
+                    <p>{category}</p>
+                    <p className="flex items-center"><CiStar /> <span> {rating}</span></p>
                 </div>
             </div>
         </div>
